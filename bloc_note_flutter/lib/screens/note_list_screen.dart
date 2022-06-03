e4778d739ca9482816cb3b5ae61a0e37e9952303
nicolas <nicolas.rossat@hotmail.fr>
2022-06-03 12:22:56 +0200

    Construction page "liste des notes" -> Design & fonc

---
+++ bloc_note_flutter/lib/screens/note_list_screen.dart
@@ -0,0 +1,122 @@
+import 'package:bloc_note_flutter/utils/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/gestures.dart';
+
+import '../helper/note_provider.dart';
+import 'note_edit_screen.dart';
+
+class NoteListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Provider.of<NoteProvider>(context, listen: false).getNotes(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+              body: Consumer<NoteProvider>(
+                child: noNotesUI(context),
+                builder: (context, noteprovider, child) =>
+                    noteprovider.items.length <= 0 ? child : Container(),
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                  goToNoteEditScreen(context);
+                },
+                child: Icon(Icons.add),
+              ),
+            );
+          }
+        }
+        return Container(
+          width: 0.0,
+          height: 0.0,
+        );
+      },
+    );
+  }
+
+  Widget header() {
+    return GestureDetector(
+      onTap: _launchUrl,
+      child: Container(
+        decoration: BoxDecoration(
+          color: headerColor,
+        ),
+        height: 100,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Bloc notes',
+              style: headerNotesStyle,
+            ),
+            Text(
+              'Nicolas ROSSAT',
+              style: headerRideStyle,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _launchUrl() async {
+    const url = 'https://github.com/NicolasROSSAT69/BlocNote';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Impossible de lancer $url';
+    }
+  }
+
+  Widget noNotesUI(BuildContext context) {
+    return ListView(
+      children: [
+        header(),
+        Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: Image.asset(
+                'crying_emoji.png',
+                fit: BoxFit.cover,
+                width: 200,
+                height: 200,
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                style: noNotesStyle,
+                children: [
+                  TextSpan(text: ' Pas de note\nTape sur "'),
+                  TextSpan(
+                      text: '+',
+                      style: boldPlus,
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          goToNoteEditScreen(context);
+                        }),
+                  TextSpan(text: '" nouvelle note'),
+                ],
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+
+  void goToNoteEditScreen(BuildContext context) {
+    Navigator.of(context).pushNamed(NoteEditScreen.route);
+  }
+}
